65ee9ccbd6295a1fa956cdd1a90b7d7153411dea
Evgeny Temryazansky <evgenytemr@gmail.com>
2023-06-28 11:14:11 +0400

    mock data for card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import './App.css';
+import './global.scss';
+import s from './App.module.scss'
+import { Card } from 'components';
+import { ICardInfo } from 'components/Card/Card';
+
+const mockData: ICardInfo = {
+  title: 'Competitor research',
+  category: {
+    title: 'UX Stages',
+    color: 'orange',
+  },
+  description: 'Research competitors and identify weakness and strengths each of them. Comparing their product features, quality...',
+  progress: {
+    done: 3,
+    amount: 8,
+  },
+  members: [
+    {
+      id: 1,
+      imageUrl: 'ava1.png',
+    },
+    {
+      id: 2,
+      imageUrl: 'ava2.png'
+    },
+    {
+      id: 3,
+      imageUrl: 'ava3.png'
+    }
+  ],
+  views: 7,
+  comments: [
+    {
+      author: 'Tommy',
+      text: 'Good job'
+    },
+    {
+      author: 'Jimmy',
+      text: 'Thanks'
+    }
+  ]
+}
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div className={s.app}>
+      <div className={s.cards}>
+        <Card cardInfo={mockData}/>
+      </div>
     </div>
   );
 }
